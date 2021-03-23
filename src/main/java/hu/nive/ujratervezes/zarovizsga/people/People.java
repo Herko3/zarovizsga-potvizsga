798c19e1e1d2025fcafdbcb4f1b3e1dc40c81f0a
Herko <herko@index.hu>
2021-03-23 12:15:23 +0100

    People done

--- src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
@@ -1,4 +1,40 @@
 package hu.nive.ujratervezes.zarovizsga.people;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class People {
+
+    public int getNumberOfMales(String path) {
+        int numberOfMales = 0;
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(path))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(",Male,")) {
+                 numberOfMales++;
+                }
+            }
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file", ioe);
+        }
+        return numberOfMales;
+    }
+
+    public long getNumberOfMales2(String path) {
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(Path.of(path));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file", ioe);
+        }
+
+        return lines.stream()
+                .filter(s->s.contains(",Male,"))
+                .count();
+    }
 }
--- src/test/java/hu/nive/ujratervezes/zarovizsga/people/PeopleTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/people/PeopleTest.java
@@ -12,4 +12,11 @@ class PeopleTest {
         int males = people.getNumberOfMales("src/test/resources/people.csv");
         assertEquals(545, males);
     }
+
+    @Test
+    void getNumberOfMales2() {
+        People people = new People();
+        long males = people.getNumberOfMales2("src/test/resources/people.csv");
+        assertEquals(545, males);
+    }
 }
