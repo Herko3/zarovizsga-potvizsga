aea736adb89c4457dc057e5e6435a40bb5feefa8
Herko <herko@index.hu>
2021-03-23 11:45:40 +0100

    Words done

--- src/main/java/hu/nive/ujratervezes/zarovizsga/words/Words.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/words/Words.java
@@ -1,4 +1,23 @@
 package hu.nive.ujratervezes.zarovizsga.words;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Words {
+
+    public boolean hasMoreDigits(String s){
+        List<Character> digits = new ArrayList<>();
+        List<Character> notDigits = new ArrayList<>();
+
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                digits.add(c);
+            } else {
+                notDigits.add(c);
+            }
+        }
+
+        return digits.size() > notDigits.size();
+    }
+
 }
