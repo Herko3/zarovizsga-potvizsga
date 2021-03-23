4fb87b4a9dec14e29be9bacabf53ba8d4af12300
Herko <herko@index.hu>
2021-03-23 12:07:06 +0100

    CleaningService done

--- src/main/java/hu/nive/ujratervezes/zarovizsga/cleaning/CleaningService.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/cleaning/CleaningService.java
@@ -1,4 +1,67 @@
 package hu.nive.ujratervezes.zarovizsga.cleaning;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CleaningService {
+
+    private List<Cleanable> cleanables = new ArrayList<>();
+
+    public void add(Cleanable cleanable) {
+        cleanables.add(cleanable);
+    }
+
+    public List<Cleanable> getCleanables() {
+        return cleanables;
+    }
+
+    public int cleanAll() {
+        int sum = cleanables.stream()
+                .mapToInt(Cleanable::clean)
+                .sum();
+        cleanables = Collections.emptyList();
+
+
+//        int sum = 0;
+//        Iterator<Cleanable> iterator = cleanables.iterator();
+//        while(iterator.hasNext()){
+//            Cleanable actual = iterator.next();
+//            sum+= actual.clean();
+//            iterator.remove();
+//        }
+        return sum;
+    }
+
+    public int cleanOnlyOffices() {
+        int sum = 0;
+        Iterator<Cleanable> iterator = cleanables.iterator();
+        while (iterator.hasNext()) {
+            Cleanable actual = iterator.next();
+            if (actual instanceof Office) {
+                sum += actual.clean();
+                iterator.remove();
+            }
+        }
+        return sum;
+    }
+
+    public List<Cleanable> findByAddressPart(String address) {
+        return cleanables.stream()
+                .filter(c -> c.getAddress().contains(address))
+                .collect(Collectors.toList());
+    }
+
+    public String getAddresses() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cleanables.size(); i++) {
+            sb.append(cleanables.get(i).getAddress());
+            if(i != cleanables.size()-1){
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/main/java/hu/nive/ujratervezes/zarovizsga/cleaning/Office.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/cleaning/Office.java
@@ -1,4 +1,28 @@
 package hu.nive.ujratervezes.zarovizsga.cleaning;
 
-public class Office {
+public class Office implements Cleanable {
+
+    private String address;
+    private int area;
+    private int floor;
+
+    public Office(String address, int area, int floor) {
+        this.address = address;
+        this.area = area;
+        this.floor = floor;
+    }
+
+    @Override
+    public int clean() {
+        return area * floor * 100;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    public int getArea() {
+        return area;
+    }
 }
